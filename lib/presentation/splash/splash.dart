3b5ac578b0a76bf1bd39a894afe01841ae7e36cd
OskarPerliński <oskar.perlinskii@gmail.com>
2025-01-03 14:12:42 +0100

    added the Logo

--- lib/presentation/splash/splash.dart
+++ lib/presentation/splash/splash.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends HookWidget {
   const SplashPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      body: Center(
+        child: Row(
+          children: [
+            Text('Food'),
+            Text('Love'),
+          ],
+        ),
+      ),
+    );
   }
 }
