3df7d936dff4c8b12f51b90b4990e66e522f377d
OskarPerliński <oskar.perlinskii@gmail.com>
2025-01-03 14:54:11 +0100

    added bloc listener

--- lib/presentation/splash/splash.dart
+++ lib/presentation/splash/splash.dart
@@ -1,32 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:recipe_api/bloc/splash/splash_cubit.dart';
+import 'package:recipe_api/bloc/splash/splash_state.dart';
+import 'package:recipe_api/presentation/auth/sign_in.dart';
 
 class SplashPage extends HookWidget {
   const SplashPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Food',
-              style: TextStyle(
-                fontSize: 40,
-                fontWeight: FontWeight.w500,
+    return Scaffold(
+      body: BlocListener<SplashCubit, SplashState>(
+        listener: (context, state) {
+          if (state is SplashUnAuthenticated) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const SignInPage(),
               ),
-            ),
-            Text(
-              'Love',
-              style: TextStyle(
-                fontSize: 40,
-                fontWeight: FontWeight.bold,
-                color: Colors.green,
+            );
+          }
+        },
+        child: const Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Food',
+                style: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.w500,
+                ),
               ),
-            ),
-          ],
+              Text(
+                'Love',
+                style: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.green,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
