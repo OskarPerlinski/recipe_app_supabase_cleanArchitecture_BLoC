c27cc0365887c87f0a7fafbbe46e0333b413a835
OskarPerliński <oskar.perlinskii@gmail.com>
2025-01-04 10:03:53 +0100

    added the UI of the page

--- lib/presentation/auth/sign_in.dart
+++ lib/presentation/auth/sign_in.dart
@@ -1,11 +1,133 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:recipe_api/commom/widgets/buttons/basic_button.dart';
 
 class SignInPage extends HookWidget {
   const SignInPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    final passwordVisible = useState(true);
+
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          alignment: Alignment.center,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              _loginText(),
+              const SizedBox(height: 50),
+              _emailField(),
+              const SizedBox(height: 20),
+              _passwordField(passwordVisible),
+              const SizedBox(height: 50),
+              _signInButton(),
+              const SizedBox(height: 15),
+              _forgotPassword(),
+              const SizedBox(height: 100),
+              _createAccount(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _loginText() {
+    return const Center(
+      child: Text(
+        'Login',
+        style: TextStyle(
+          fontSize: 35,
+          fontWeight: FontWeight.bold,
+          color: Colors.green,
+        ),
+      ),
+    );
+  }
+
+  Widget _emailField() {
+    return const Padding(
+      padding: EdgeInsets.only(left: 30, right: 30),
+      child: TextField(
+        decoration: InputDecoration(
+          hintText: 'email',
+          hintStyle: TextStyle(color: Colors.grey),
+          border: OutlineInputBorder(),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.green, width: 2),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _passwordField(ValueNotifier<bool> passwordVisible) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 30, right: 30),
+      child: TextField(
+        obscureText: passwordVisible.value,
+        decoration: InputDecoration(
+          hintText: 'password',
+          hintStyle: const TextStyle(color: Colors.grey),
+          suffixIcon: IconButton(
+            onPressed: () {
+              passwordVisible.value = !passwordVisible.value;
+            },
+            icon: Icon(passwordVisible.value
+                ? Icons.visibility_off
+                : Icons.visibility),
+          ),
+          border: const OutlineInputBorder(),
+          focusedBorder: const OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.green, width: 2),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _signInButton() {
+    return Padding(
+      padding: const EdgeInsets.only(left: 30, right: 30),
+      child: BasicButton(
+        onPressed: () {},
+        title: 'Sign In',
+      ),
+    );
+  }
+
+  Widget _forgotPassword() {
+    return const Text(
+      'Forgot password?',
+      style: TextStyle(
+        fontWeight: FontWeight.bold,
+        color: Colors.purple,
+      ),
+    );
+  }
+
+  Widget _createAccount() {
+    return const Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          'Dont have an account? ',
+          style: TextStyle(
+            fontWeight: FontWeight.w500,
+            color: Colors.black38,
+          ),
+        ),
+        Text(
+          'Sign Up',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.green,
+          ),
+        )
+      ],
+    );
   }
 }
