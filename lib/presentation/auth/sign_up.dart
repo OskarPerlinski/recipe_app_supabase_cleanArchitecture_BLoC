78f678742c6e9327bbf321b930733f688f103924
OskarPerliński <oskar.perlinskii@gmail.com>
2025-01-05 20:31:50 +0100

    added TextField and Navigation

--- lib/presentation/auth/sign_up.dart
+++ lib/presentation/auth/sign_up.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:recipe_api/commom/widgets/buttons/basic_button.dart';
+import 'package:recipe_api/presentation/auth/sign_in.dart';
 
 class SignUpPage extends HookWidget {
   const SignUpPage({super.key});
@@ -20,6 +21,8 @@ class SignUpPage extends HookWidget {
             children: [
               _loginText(),
               const SizedBox(height: 50),
+              _nameField(),
+              const SizedBox(height: 20),
               _emailField(),
               const SizedBox(height: 20),
               _passwordField(passwordVisible),
@@ -28,7 +31,7 @@ class SignUpPage extends HookWidget {
               const SizedBox(height: 50),
               _createAccButton(),
               const SizedBox(height: 100),
-              _loginToAcc(),
+              _loginToAcc(context),
             ],
           ),
         ),
@@ -44,6 +47,22 @@ class SignUpPage extends HookWidget {
           fontSize: 35,
           fontWeight: FontWeight.bold,
           color: Colors.green,
+        ),
+      ),
+    );
+  }
+
+  Widget _nameField() {
+    return const Padding(
+      padding: EdgeInsets.only(left: 30, right: 30),
+      child: TextField(
+        decoration: InputDecoration(
+          hintText: 'name',
+          hintStyle: TextStyle(color: Colors.grey),
+          border: OutlineInputBorder(),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.green, width: 2),
+          ),
         ),
       ),
     );
@@ -125,22 +144,32 @@ class SignUpPage extends HookWidget {
     );
   }
 
-  Widget _loginToAcc() {
-    return const Row(
+  Widget _loginToAcc(BuildContext context) {
+    return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Text(
+        const Text(
           'Already have an account? ',
           style: TextStyle(
             fontWeight: FontWeight.w500,
             color: Colors.black38,
           ),
         ),
-        Text(
-          'Sign In',
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            color: Colors.green,
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const SignInPage(),
+              ),
+            );
+          },
+          child: const Text(
+            'Sign In',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.green,
+            ),
           ),
         )
       ],
