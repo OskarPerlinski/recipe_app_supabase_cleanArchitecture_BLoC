9ff7f7041cba976ab4b9493857f3b8fa650d6035
OskarPerliński <oskar.perlinskii@gmail.com>
2025-01-05 20:34:39 +0100

    create ResetPassword Page

---
+++ lib/presentation/auth/reset_password.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+class ResetPasswordPage extends HookWidget {
+  const ResetPasswordPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
