cea0a7e6d9ce22eec5798c0944497204973f4d2a
OskarPerliński <oskar.perlinskii@gmail.com>
2025-01-07 21:08:39 +0100

    added repositoryimpl to sign in

--- lib/data/auth/repository/auth.dart
+++ lib/data/auth/repository/auth.dart
@@ -1,5 +1,6 @@
 import 'package:dartz/dartz.dart';
 import 'package:recipe_api/data/auth/model/user_creation_req.dart';
+import 'package:recipe_api/data/auth/model/user_signin_req.dart';
 import 'package:recipe_api/data/auth/source/auth_supabase_service.dart';
 import 'package:recipe_api/domain/auth/repository/auth.dart';
 import 'package:recipe_api/service_locator.dart';
@@ -8,6 +9,11 @@ class AuthRepositoryImpl extends AuthRepository{
   @override
   Future<Either> getSignUp(UserCreationReq user) async {
     return await sl<AuthSupabaseService>().getSignUp(user);
+  }
+
+  @override
+  Future<Either> getSignIn(UserSigninReq user) async {
+    return await sl<AuthSupabaseService>().getSignIn(user);
   }
   
 }
