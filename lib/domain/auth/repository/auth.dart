2602ee5259e690fe1e09e6cfa2c381d93089a4cc
OskarPerliński <oskar.perlinskii@gmail.com>
2025-01-07 21:09:32 +0100

    added sign in to repository

--- lib/domain/auth/repository/auth.dart
+++ lib/domain/auth/repository/auth.dart
@@ -1,6 +1,8 @@
 import 'package:dartz/dartz.dart';
 import 'package:recipe_api/data/auth/model/user_creation_req.dart';
+import 'package:recipe_api/data/auth/model/user_signin_req.dart';
 
 abstract class AuthRepository {
   Future<Either> getSignUp(UserCreationReq user);
+  Future<Either> getSignIn(UserSigninReq user);
 }
