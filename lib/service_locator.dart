ed19cea75b44b8b62b36b954729bcf334944e8d3
OskarPerliński <oskar.perlinskii@gmail.com>
2025-01-07 18:41:23 +0100

    create and add auth service, repo, and signup usecase

---
+++ lib/service_locator.dart
@@ -0,0 +1,17 @@
+import 'package:get_it/get_it.dart';
+import 'package:recipe_api/data/auth/repository/auth.dart';
+import 'package:recipe_api/data/auth/source/auth_supabase_service.dart';
+import 'package:recipe_api/domain/auth/repository/auth.dart';
+import 'package:recipe_api/domain/auth/usecases/get_sign_up.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+final sl = GetIt.instance;
+
+Future<void> initializeDependencies() async {
+  //services
+  sl.registerSingleton<AuthSupabaseService>(AuthSupabaseServiceImpl(supabaseClient: Supabase.instance.client));
+  //repository
+  sl.registerSingleton<AuthRepository>(AuthRepositoryImpl());
+  //usecases
+  sl.registerSingleton<GetSignUpUseCase>(GetSignUpUseCase());
+}
