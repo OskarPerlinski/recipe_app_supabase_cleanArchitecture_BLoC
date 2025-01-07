d712b804983ff61552d47b37b04c6ca9aef06a27
OskarPerliński <oskar.perlinskii@gmail.com>
2025-01-07 18:42:52 +0100

    create repository for signUp

---
+++ lib/data/auth/repository/auth.dart
@@ -0,0 +1,13 @@
+import 'package:dartz/dartz.dart';
+import 'package:recipe_api/data/auth/model/user_creation_req.dart';
+import 'package:recipe_api/data/auth/source/auth_supabase_service.dart';
+import 'package:recipe_api/domain/auth/repository/auth.dart';
+import 'package:recipe_api/service_locator.dart';
+
+class AuthRepositoryImpl extends AuthRepository{
+  @override
+  Future<Either> getSignUp(UserCreationReq user) async {
+    return await sl<AuthSupabaseService>().getSignUp(user);
+  }
+  
+}
