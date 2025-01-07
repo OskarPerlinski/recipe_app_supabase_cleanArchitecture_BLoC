8b694f11116bcb6104b239489bc06e71733a9ae7
OskarPerliński <oskar.perlinskii@gmail.com>
2025-01-07 21:09:00 +0100

    added source to sign in

--- lib/data/auth/source/auth_supabase_service.dart
+++ lib/data/auth/source/auth_supabase_service.dart
@@ -1,9 +1,11 @@
 import 'package:dartz/dartz.dart';
 import 'package:recipe_api/data/auth/model/user_creation_req.dart';
+import 'package:recipe_api/data/auth/model/user_signin_req.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 abstract class AuthSupabaseService {
   Future<Either> getSignUp(UserCreationReq user);
+  Future<Either> getSignIn(UserSigninReq user);
 }
 
 class AuthSupabaseServiceImpl extends AuthSupabaseService {
@@ -19,9 +21,20 @@ class AuthSupabaseServiceImpl extends AuthSupabaseService {
       );
       return Right(response.user!.id);
     } catch (e) {
-      return const Left(
-        'Please try again'
+      return const Left('Please try again');
+    }
+  }
+
+  @override
+  Future<Either> getSignIn(UserSigninReq user) async {
+    try {
+      final response = await supabaseClient.auth.signInWithPassword(
+        password: user.password!,
+        email: user.email!,
       );
+      return Right(response.user!.id);
+    } catch (e) {
+      return const Left('Please try again');
     }
   }
 }
