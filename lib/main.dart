7a5437e62f9fce0552b17db114a0599f1ec487f9
OskarPerliński <oskar.perlinskii@gmail.com>
2025-01-07 17:06:57 +0100

    initialize supabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:recipe_api/bloc/splash/splash_cubit.dart';
 import 'package:recipe_api/presentation/splash/splash.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load();
+
+   await Supabase.initialize(
+    url: dotenv.env['SUPABASE_API_URL']!,
+    anonKey: dotenv.env['SUPABASE_API_KEY']!,
+  );
+
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
