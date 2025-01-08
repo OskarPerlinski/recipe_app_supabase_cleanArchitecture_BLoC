89aa00ee42c4d01d8686e0dd4c24db538741d585
OskarPerliński <oskar.perlinskii@gmail.com>
2025-01-08 17:09:17 +0100

    add header widget to homePage

--- lib/presentation/home/home.dart
+++ lib/presentation/home/home.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:recipe_api/presentation/home/widgtes/header.dart';
 
 class HomePage extends HookWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      body: Column(
+        children: [
+          Header(),
+        ],
+      ),
+    );
   }
 }
