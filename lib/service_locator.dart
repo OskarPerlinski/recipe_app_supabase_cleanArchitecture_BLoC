6017aa6d040f6b5d9e9b0fb24f7833bc8c2ffd7f
OskarPerliński <oskar.perlinskii@gmail.com>
2025-01-08 15:12:19 +0100

    added get_reset_password to service_locator

--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -2,6 +2,7 @@ import 'package:get_it/get_it.dart';
 import 'package:recipe_api/data/auth/repository/auth.dart';
 import 'package:recipe_api/data/auth/source/auth_supabase_service.dart';
 import 'package:recipe_api/domain/auth/repository/auth.dart';
+import 'package:recipe_api/domain/auth/usecases/get_reset_password.dart';
 import 'package:recipe_api/domain/auth/usecases/get_sign_in.dart';
 import 'package:recipe_api/domain/auth/usecases/get_sign_up.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
@@ -16,4 +17,5 @@ Future<void> initializeDependencies() async {
   //usecases
   sl.registerSingleton<GetSignUpUseCase>(GetSignUpUseCase());
   sl.registerSingleton<GetSignInUseCase>(GetSignInUseCase());
+  sl.registerSingleton<GetResetPasswordUseCase>(GetResetPasswordUseCase());
 }
