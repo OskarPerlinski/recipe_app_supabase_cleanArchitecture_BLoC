a8bf45e5fb3cd7a9f4a10e16bb7bf89bf68d01f1
OskarPerliński <oskar.perlinskii@gmail.com>
2025-01-08 15:59:42 +0100

    added isLoggedIn

--- lib/bloc/splash/splash_cubit.dart
+++ lib/bloc/splash/splash_cubit.dart
@@ -1,11 +1,18 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:recipe_api/bloc/splash/splash_state.dart';
+import 'package:recipe_api/domain/auth/usecases/is_logge_in.dart';
+import 'package:recipe_api/service_locator.dart';
 
 class SplashCubit extends Cubit<SplashState> {
   SplashCubit() : super(SplashInitialState());
 
   void appStarted() async {
     await Future.delayed(const Duration(seconds: 3));
-    emit(SplashUnAuthenticated());
+    var isLoggedIn = await sl<IsLoggeInUseCase>().call();
+    if (isLoggedIn) {
+      emit(SplashAuthenticated());
+    } else {
+      emit(SplashUnAuthenticated());
+    }
   }
 }
