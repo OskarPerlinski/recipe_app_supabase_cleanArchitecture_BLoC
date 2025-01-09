a78aa115e075d825338211d01dc818f3a2190d41
OskarPerliński <oskar.perlinskii@gmail.com>
2025-01-09 21:43:20 +0100

    added breakfast and lunch

--- lib/presentation/home/home.dart
+++ lib/presentation/home/home.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:recipe_api/presentation/home/widgtes/breakfast.dart';
 import 'package:recipe_api/presentation/home/widgtes/categories.dart';
 import 'package:recipe_api/presentation/home/widgtes/description.dart';
 import 'package:recipe_api/presentation/home/widgtes/header.dart';
+import 'package:recipe_api/presentation/home/widgtes/lunch.dart';
 
 class HomePage extends HookWidget {
   const HomePage({super.key});
@@ -10,14 +12,20 @@ class HomePage extends HookWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Column(
-        children: [
-          Header(),
-          SizedBox(height: 50),
-          Description(),
-          SizedBox(height: 30),
-          Categories(),
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Header(),
+            SizedBox(height: 50),
+            Description(),
+            SizedBox(height: 30),
+            Categories(),
+            SizedBox(height: 50),
+            Breakfast(),
+            SizedBox(height: 30),
+            Lunch(),
+          ],
+        ),
       ),
     );
   }
