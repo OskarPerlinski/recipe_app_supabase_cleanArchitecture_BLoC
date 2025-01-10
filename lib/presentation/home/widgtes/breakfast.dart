4cf23310abaa0e463b8c6fb7e92df18ac0234f63
OskarPerliński <oskar.perlinskii@gmail.com>
2025-01-10 20:14:09 +0100

    added bloc provider, builder

--- lib/presentation/home/widgtes/breakfast.dart
+++ lib/presentation/home/widgtes/breakfast.dart
@@ -1,18 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:recipe_api/bloc/recipe/recipe_cubit.dart';
+import 'package:recipe_api/bloc/recipe/recipe_state.dart';
 import 'package:recipe_api/commom/widgets/recipe_card/recipe_card.dart';
+import 'package:recipe_api/domain/recipe/entity/recipe.dart';
+import 'package:recipe_api/domain/recipe/usecases/get_breakfast.dart';
+import 'package:recipe_api/service_locator.dart';
 
 class Breakfast extends HookWidget {
   const Breakfast({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        _breakfastText(),
-        const SizedBox(height: 30),
-        _recipeCard(),
-      ],
+    return BlocProvider(
+      create: (context) => RecipeCubit(useCase: sl<GetBreakfastUseCase>())..displayRecipe(),
+      child: BlocBuilder<RecipeCubit, RecipeState>(
+        builder: (context, state) {
+          if (state is RecipeLoading) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          if (state is RecipeLoaded) {
+            return Column(
+              children: [
+                _breakfastText(),
+                const SizedBox(height: 30),
+                _recipeCard(state.recipe),
+              ],
+            );
+          }
+          if (state is FailureLoadedRecipe) {
+            const Center(
+              child: Text(
+                'Failed to load recipes.',
+                style: TextStyle(color: Colors.red),
+              ),
+            );
+          }
+          return Container();
+        },
+      ),
     );
   }
 
@@ -27,14 +56,13 @@ class Breakfast extends HookWidget {
               fontWeight: FontWeight.bold,
               fontSize: 20,
             ),
-            
           ),
         ],
       ),
     );
   }
 
-  Widget _recipeCard() {
+  Widget _recipeCard(List<RecipeEntity> recipe) {
     return Padding(
       padding: const EdgeInsets.only(left: 20, right: 20),
       child: SizedBox(
@@ -43,10 +71,10 @@ class Breakfast extends HookWidget {
           shrinkWrap: true,
           scrollDirection: Axis.horizontal,
           itemBuilder: (context, index) {
-            return const RecipeCard();
+            return RecipeCard(recipeEntity: recipe[index],);
           },
-          separatorBuilder: (context, index) =>  const SizedBox(width: 10),
-          itemCount: 4,
+          separatorBuilder: (context, index) => const SizedBox(width: 10),
+          itemCount: recipe.length,
         ),
       ),
     );
