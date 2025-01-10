c69429bb1014143acef93c5d51d62828f7b31dcc
OskarPerliński <oskar.perlinskii@gmail.com>
2025-01-10 20:09:11 +0100

    create usecase for getBreakfast

---
+++ lib/domain/recipe/usecases/get_breakfast.dart
@@ -0,0 +1,12 @@
+import 'package:dartz/dartz.dart';
+import 'package:recipe_api/core/config/usecases/usecase.dart';
+import 'package:recipe_api/domain/recipe/repository/recipe.dart';
+import 'package:recipe_api/service_locator.dart';
+
+class GetBreakfastUseCase implements UseCase<Either, dynamic> {
+  @override
+  Future<Either> call({dynamic params}) async {
+    return await sl<RecipeRepository>().getBreakfast();
+  }
+
+}
