d99c1977e4415cc33f2d6a12fd82a8bf7f305fe5
OskarPerliński <oskar.perlinskii@gmail.com>
2025-01-10 20:07:46 +0100

    create repositoryImpl for recipe

---
+++ lib/data/recipe/repository/recipe.dart
@@ -0,0 +1,22 @@
+import 'package:dartz/dartz.dart';
+import 'package:recipe_api/data/recipe/models/recipe.dart';
+import 'package:recipe_api/data/recipe/source/recipe_supabase_service.dart';
+import 'package:recipe_api/domain/recipe/repository/recipe.dart';
+import 'package:recipe_api/service_locator.dart';
+
+class RecipeRepositoryImpl extends RecipeRepository {
+  @override
+  Future<Either> getBreakfast() async {
+    var retunredData = await sl<RecipeSupabaseService>().getBreakfast();
+    return retunredData.fold(
+     (error){
+        return Left(error);
+      },
+      (data){
+        return Right(
+          List.from(data).map((e) => RecipeModels.fromMap(e).toEntity()).toList()
+        );
+      },
+    );
+  }
+}
