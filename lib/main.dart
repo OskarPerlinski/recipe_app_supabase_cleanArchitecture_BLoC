565bb5706a529c4d7cc5cb3bf34b827b810c6a26
OskarPerliński <oskar.perlinskii@gmail.com>
2025-01-11 12:52:09 +0100

    added bloc provider

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'package:recipe_api/bloc/recipe/recipe_cubit.dart';
 import 'package:recipe_api/bloc/splash/splash_cubit.dart';
 import 'package:recipe_api/domain/recipe/usecases/get_breakfast.dart';
 import 'package:recipe_api/domain/recipe/usecases/get_high_protein.dart';
+import 'package:recipe_api/domain/recipe/usecases/get_meal.dart';
 import 'package:recipe_api/presentation/splash/splash.dart';
 import 'package:recipe_api/service_locator.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
@@ -42,6 +43,7 @@ class MyApp extends StatelessWidget {
         BlocProvider(create: (context) => CategoryCubit()..displayCategory()),
         BlocProvider(create: (context) => RecipeCubit(useCase: sl<GetBreakfastUseCase>())),
         BlocProvider(create: (context) => RecipeCubit(useCase: sl<GetHighProteinUseCase>())),
+        BlocProvider(create: (context) => RecipeCubit(useCase: sl<GetMealUseCase>())),
       ],
       child: MaterialApp(
         title: 'Flutter Demo',
