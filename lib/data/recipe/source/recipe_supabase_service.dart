3fa1cb2742abe005ef0b34af23dc668b139cf09d
OskarPerliński <oskar.perlinskii@gmail.com>
2025-01-11 12:54:13 +0100

    added getMeal to source

--- lib/data/recipe/source/recipe_supabase_service.dart
+++ lib/data/recipe/source/recipe_supabase_service.dart
@@ -4,30 +4,46 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 abstract class RecipeSupabaseService {
   Future<Either> getBreakfast();
   Future<Either> getHighProtein();
+  Future<Either> getMeal();
 }
 
 class RecipeSupabaseServiceImpl extends RecipeSupabaseService {
   final SupabaseClient supabaseClient;
   RecipeSupabaseServiceImpl({required this.supabaseClient});
   @override
-
   Future<Either> getBreakfast() async {
-    try{
-      var returnedData = await supabaseClient.from('recipe').select().eq('category', 'breakfast');
+    try {
+      var returnedData = await supabaseClient
+          .from('recipe')
+          .select()
+          .eq('category', 'breakfast');
       return Right(returnedData);
     } catch (e) {
       return const Left('Please try again');
     }
   }
-  
+
   @override
   Future<Either> getHighProtein() async {
-    try{
-      var retunredData = await supabaseClient.from('recipe').select().eq('category', 'highProtein');
+    try {
+      var retunredData = await supabaseClient
+          .from('recipe')
+          .select()
+          .eq('category', 'highProtein');
       return Right(retunredData);
-    } catch(e) {
+    } catch (e) {
       return const Left('Please try again');
     }
   }
 
+  @override
+  Future<Either> getMeal() async {
+    try {
+      var retunredData =
+          await supabaseClient.from('recipe').select().eq('category', 'meal');
+      return Right(retunredData);
+    } catch (e) {
+      return const Left('Please try again');
+    }
+  }
 }
