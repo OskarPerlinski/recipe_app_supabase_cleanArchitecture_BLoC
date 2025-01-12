36e98a6e1c05b711e941c07183a06986a9ff535c
OskarPerliński <oskar.perlinskii@gmail.com>
2025-01-12 15:44:50 +0100

    added categoryEntity

--- lib/presentation/category_page/category_page.dart
+++ lib/presentation/category_page/category_page.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:recipe_api/commom/widgets/appbar/basic_appbar.dart';
+import 'package:recipe_api/domain/category/entity/category.dart';
 
 class CategoryPage extends HookWidget {
-  const CategoryPage({super.key});
+  final CategoryEntity categoryEntity;
+  const CategoryPage({super.key, required this.categoryEntity});
 
   @override
   Widget build(BuildContext context) {
