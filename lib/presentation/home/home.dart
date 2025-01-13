fbe07dcecf905df138cb48f3e6dd4eda7f2f8888
OskarPerliński <oskar.perlinskii@gmail.com>
2025-01-13 09:04:20 +0100

    added bottom tapbar

--- lib/presentation/home/home.dart
+++ lib/presentation/home/home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:recipe_api/commom/widgets/basic_tapbar/basic_tapbar.dart';
+import 'package:recipe_api/presentation/favourties/favourites.dart';
 import 'package:recipe_api/presentation/home/widgtes/breakfast.dart';
 import 'package:recipe_api/presentation/home/widgtes/categories.dart';
 import 'package:recipe_api/presentation/home/widgtes/description.dart';
@@ -12,8 +14,8 @@ class HomePage extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SingleChildScrollView(
+    return Scaffold(
+      body: const SingleChildScrollView(
         child: Column(
           children: [
             Header(),
@@ -30,6 +32,22 @@ class HomePage extends HookWidget {
             SizedBox(height: 30),
           ],
         ),
+      ),
+      bottomNavigationBar: BasicTapbar(
+        currentIndex: 0,
+        onTap: (index) {
+          if (index == 0) {
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (context) => const HomePage()),
+            );
+          } else if (index == 1) {
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (context) => const FavouritesPage()),
+            );
+          }
+        },
       ),
     );
   }
