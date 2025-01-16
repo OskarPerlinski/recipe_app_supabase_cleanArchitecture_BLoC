4a027c1cce2da0f2acaac2760f122b575bad1f73
OskarPerliński <oskar.perlinskii@gmail.com>
2025-01-16 11:35:14 +0100

    added repository, source, usecase for favorite

--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -3,6 +3,8 @@ import 'package:recipe_api/data/auth/repository/auth.dart';
 import 'package:recipe_api/data/auth/source/auth_supabase_service.dart';
 import 'package:recipe_api/data/category/repository/category.dart';
 import 'package:recipe_api/data/category/source/category_supabase_service.dart';
+import 'package:recipe_api/data/favorite/repository/favorite.dart';
+import 'package:recipe_api/data/favorite/source/favorite_supabase_source.dart';
 import 'package:recipe_api/data/recipe/repository/recipe.dart';
 import 'package:recipe_api/data/recipe/source/recipe_supabase_service.dart';
 import 'package:recipe_api/domain/auth/repository/auth.dart';
@@ -13,6 +15,10 @@ import 'package:recipe_api/domain/auth/usecases/get_sign_up.dart';
 import 'package:recipe_api/domain/auth/usecases/is_logge_in.dart';
 import 'package:recipe_api/domain/category/repository/category.dart';
 import 'package:recipe_api/domain/category/usecases/get_category.dart';
+import 'package:recipe_api/domain/favorite/repository/favorite.dart';
+import 'package:recipe_api/domain/favorite/usecase/add_or_remove_favorite_recipe.dart';
+import 'package:recipe_api/domain/favorite/usecase/get_favorites.dart';
+import 'package:recipe_api/domain/favorite/usecase/is_favorite.dart';
 import 'package:recipe_api/domain/recipe/repository/recipe.dart';
 import 'package:recipe_api/domain/recipe/usecases/get_breakfast.dart';
 import 'package:recipe_api/domain/recipe/usecases/get_by_category_id.dart';
@@ -27,10 +33,12 @@ Future<void> initializeDependencies() async {
   sl.registerSingleton<AuthSupabaseService>(AuthSupabaseServiceImpl(supabaseClient: Supabase.instance.client));
   sl.registerSingleton<CategorySupabaseService>(CategorySupabaseServiceImpl(supabaseClient: Supabase.instance.client));
   sl.registerSingleton<RecipeSupabaseService>(RecipeSupabaseServiceImpl(supabaseClient: Supabase.instance.client));
+  sl.registerSingleton<FavoriteSupabaseSource>(FavoriteSupabaseSourceImpl(supabaseClient: Supabase.instance.client));
   //repository
   sl.registerSingleton<AuthRepository>(AuthRepositoryImpl());
   sl.registerSingleton<CategoryRepository>(CategoryRepositoryImpl());
   sl.registerSingleton<RecipeRepository>(RecipeRepositoryImpl());
+  sl.registerSingleton<FavoriteRepository>(FavoriteRepositoryImpl());
   //usecases
   sl.registerSingleton<GetSignUpUseCase>(GetSignUpUseCase());
   sl.registerSingleton<GetSignInUseCase>(GetSignInUseCase());
@@ -42,4 +50,7 @@ Future<void> initializeDependencies() async {
   sl.registerSingleton<GetHighProteinUseCase>(GetHighProteinUseCase());
   sl.registerSingleton<GetMealUseCase>(GetMealUseCase());
   sl.registerSingleton<GetByCategoryIdUseCase>(GetByCategoryIdUseCase());
+  sl.registerSingleton<AddOrRemoveFavoriteRecipeUseCase>(AddOrRemoveFavoriteRecipeUseCase());
+  sl.registerSingleton<GetFavoritesUseCase>(GetFavoritesUseCase());
+  sl.registerSingleton<IsFavoriteUseCase>(IsFavoriteUseCase());
 }
